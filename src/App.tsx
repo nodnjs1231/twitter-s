ed497934d1f460882dadf292bdb2fbc303188212
WonJaeNo <nodnjs1231@quve.kr>
2024-10-30 20:29:58 +0900

    Add : Firebase 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import Layout from 'components/Layout'
-import Router from 'components/Router'
+import Layout from 'components/Layout';
+import Router from 'components/Router';
+
+import { getAuth } from 'firebase/auth';
+import { app } from 'firebaseApp';
+import { useState } from 'react';
 
 function App() {
+  const auth = getAuth(app);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
   return (
     <Layout>
       <Router />
     </Layout>
-  )
+  );
 }
 
 export default App;
