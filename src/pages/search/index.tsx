67e93bfa76b6755eafaab2966c4192d4074da70f
WonJaeNo <nodnjs1231@quve.kr>
2024-11-05 18:16:11 +0900

    Add : 해시태그 검색

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,6 +1,70 @@
-export default function SearchPage(){
-    
-    return(
-        <div>SearchPage</div>
-    );
+import { AuthContext } from 'components/context/AuthContext';
+import PostBox from 'components/posts/PostBox';
+import {
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from 'firebase/firestore';
+import { db } from 'firebaseApp';
+import { PostProps } from 'pages/home';
+import { useContext, useEffect, useState } from 'react';
+
+export default function SearchPage() {
+  const { user } = useContext(AuthContext);
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const [tagQuery, setTagQuery] = useState<string>('');
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTagQuery(e?.target?.value?.trim());
+  };
+
+  useEffect(() => {
+    if (user) {
+      let postsRef = collection(db, 'posts');
+      let postsQuery = query(
+        postsRef,
+        where('hashTags', 'array-contains-any', [tagQuery]), //where(필드, 배열에 포함된 모든값, 검색배열))
+        orderBy('createdAt', 'desc')
+      );
+
+      onSnapshot(postsQuery, (snapShot) => {
+        let dataObj = snapShot?.docs?.map((doc) => ({
+          ...doc?.data(),
+          id: doc?.id,
+        }));
+
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [tagQuery, user]);
+
+  return (
+    <div className="home">
+      <div className="home__top">
+        <div className="home__title">
+          <div className="home__title-text">Search</div>
+        </div>
+        <div className="home__search-div">
+          <input
+            className="home__search"
+            placeholder="해시태그 검색"
+            onChange={onChange}
+            value={tagQuery}
+          />
+        </div>
+      </div>
+      <div className="post">
+        {posts?.length > 0 ? (
+          posts?.map((post) => <PostBox post={post} key={post.id} />)
+        ) : (
+          <div className="post__no-posts">
+            <div className="post__text">게시글이 없습니다.</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
