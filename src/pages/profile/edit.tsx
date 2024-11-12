a8c42a720b804b8fbf04307adb27f60132313c3c
WonJaeNo <nodnjs1231@quve.kr>
2024-11-12 19:11:53 +0900

    Add : 프로필 이미지 수정

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -1,6 +1,150 @@
-export default function ProfileEdit(){
-    
-    return(
-        <div>Profile Edit Page</div>
-    );
+import { AuthContext } from 'components/context/AuthContext';
+import PostHeader from 'components/posts/PostHeader';
+import { updateProfile } from 'firebase/auth';
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadString,
+} from 'firebase/storage';
+import { storage } from 'firebaseApp';
+import { useContext, useEffect, useState } from 'react';
+import { FiImage } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { v4 as uuidv4 } from 'uuid';
+
+export default function ProfileEdit() {
+  const [disPlayName, setDisPlayName] = useState<string>('');
+  const [imageFile, setImageFile] = useState<string | null>(null);
+  const { user } = useContext(AuthContext);
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = e;
+    setDisPlayName(value);
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    const key = `${user?.uid}/${uuidv4()}`;
+    // 참조 생성
+    const storageRef = ref(storage, key);
+
+    try {
+      // 기존 이미지 삭제
+      if (user?.photoURL) {
+        const imageRef = ref(storage, user?.photoURL);
+        await deleteObject(imageRef).catch((error) => {
+          console.log(error);
+        });
+      }
+      // 이미지 업로드
+      let newImageUrl = null;
+      if (imageFile) {
+        const data = await uploadString(storageRef, imageFile, 'data_url');
+        newImageUrl = await getDownloadURL(data?.ref);
+      }
+
+      // updateProfile 호출
+      if (user) {
+        await updateProfile(user, {
+          displayName: disPlayName || '',
+          photoURL: newImageUrl || '',
+        }).then(() => {
+          setImageFile(null);
+          toast?.success('프로필을 업데이트 했습니다.');
+          navigate(`/profile`);
+        });
+      }
+    } catch (e: any) {
+      console.log(e);
+      toast?.error(e?.code);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+  const handleFileUpload = (e: any) => {
+    e.preventDefault();
+    const {
+      target: { files },
+    } = e;
+
+    const file = files?.[0];
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onloadend = (e: any) => {
+      const { result } = e?.currentTarget;
+      setImageFile(result);
+    };
+  };
+
+  const handleDeleteImage = () => {
+    setImageFile(null);
+  };
+
+  useEffect(() => {
+    if (user?.photoURL) {
+      setImageFile(user?.photoURL);
+    }
+    if (user?.displayName) {
+      setDisPlayName(user?.displayName);
+    }
+  }, [user?.photoURL, user?.displayName]);
+
+  return (
+    <div className="post">
+      <PostHeader />
+      <form className="post-form" onSubmit={onSubmit}>
+        <div className="post-form__profile">
+          <input
+            type="text"
+            name="disPlayName"
+            className="post-form__input"
+            placeholder="이름"
+            onChange={onChange}
+            value={disPlayName}
+          />
+          {imageFile && (
+            <div className="post-form__attacthment">
+              <img src={imageFile} alt="attacthment" width={100} height={100} />
+              <button
+                className="post-form__clear-btn"
+                type="button"
+                onClick={handleDeleteImage}
+              >
+                삭제
+              </button>
+            </div>
+          )}
+          <div className="post-form__submit-area">
+            <div className="post-form__image-area">
+              <label className="post-form__file" htmlFor="file-input">
+                <FiImage className="post-form__file-icon" />
+              </label>
+              <input
+                type="file"
+                name="file-input"
+                id="file-input"
+                accept="image/*"
+                className="hidden"
+                onChange={handleFileUpload}
+              />
+            </div>
+            <input
+              type="submit"
+              value="프로필 수정"
+              className="post-form__submit-btn"
+              disabled={isSubmitting}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,6 +1,81 @@
-export default function ProfilePage(){
-    
-    return(
-        <div>Profile Page</div>
-    );
+import { AuthContext } from 'components/context/AuthContext';
+import PostBox from 'components/posts/PostBox';
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from 'firebase/firestore';
+import { db } from 'firebaseApp';
+import { PostProps } from 'pages/home';
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const PROFILE_DEFUALT_URL = '/logo512.png';
+
+export default function ProfilePage() {
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      let postsRef = collection(db, 'posts');
+      let postQuer = query(
+        postsRef,
+        where('uid', '==', user?.uid),
+        orderBy('createdAt', 'desc')
+      );
+
+      onSnapshot(postQuer, (snapShot) => {
+        let dataObj = snapShot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc?.id,
+        }));
+
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [user]);
+  return (
+    <div className="home">
+      <div className="home__top">
+        <div className="home__title">Profile</div>
+        <div className="profile">
+          <img
+            src={user?.photoURL || PROFILE_DEFUALT_URL}
+            alt="profile"
+            className="profile__img"
+            width={100}
+            height={100}
+          />
+          <button
+            type="button"
+            className="profile__btn"
+            onClick={() => navigate('/profile/edit')}
+          >
+            프로필 수정
+          </button>
+        </div>
+        <div className="profile__text">
+          <div className="profile__name">{user?.displayName || '사용자님'}</div>
+          <div className="profile__email">{user?.email}</div>
+        </div>
+        <div className="home__tabs">
+          <div className="home__tab home__tab--active">For You</div>
+          <div className="home__tab">Likes</div>
+        </div>
+        <div className="post">
+          {posts?.length > 0 ? (
+            posts?.map((post) => <PostBox post={post} key={post.id} />)
+          ) : (
+            <div className="post__no-posts">
+              <div className="post__text">게시글이 없습니다.</div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
