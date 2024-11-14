d760ea94497f8aa1c03add89c2a13fa0b08f3975
WonJaeNo <nodnjs1231@quve.kr>
2024-11-14 18:19:12 +0900

    Add : 좋아요 기능

--- src/components/posts/PostBox.tsx
+++ src/components/posts/PostBox.tsx
@@ -1,10 +1,16 @@
 import { AuthContext } from 'components/context/AuthContext';
-import { deleteDoc, doc } from 'firebase/firestore';
+import {
+  arrayRemove,
+  arrayUnion,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from 'firebase/firestore';
 import { deleteObject, ref } from 'firebase/storage';
 import { db, storage } from 'firebaseApp';
 import { PostProps } from 'pages/home';
 import { useContext } from 'react';
-import { AiFillHeart } from 'react-icons/ai';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 import { FaRegComment, FaUserCircle } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -17,6 +23,23 @@ export default function PostBox({ post }: Props) {
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
   const imageRef = ref(storage, post?.imageUrl);
+
+  const toggleLike = async () => {
+    const postRef = doc(db, 'posts', post.id);
+    if (user?.uid && post?.likes?.includes(user?.uid)) {
+      // 사용자가 좋아요 취소일때
+      await updateDoc(postRef, {
+        likes: arrayRemove(user?.uid),
+        likeCount: post?.likeCount ? post?.likeCount - 1 : 0,
+      });
+    } else {
+      // 사용자가 좋아요 할때
+      await updateDoc(postRef, {
+        likes: arrayUnion(user?.uid),
+        likeCount: post?.likeCount ? post?.likeCount + 1 : 1,
+      });
+    }
+  };
 
   const handleDelete = async () => {
     const confirm = window.confirm('해당 게시글을 삭제하시겠습니까?');
@@ -88,8 +111,12 @@ export default function PostBox({ post }: Props) {
             </button>
           </>
         )}
-        <button type="button" className="post__likes">
-          <AiFillHeart />
+        <button type="button" className="post__likes" onClick={toggleLike}>
+          {user?.uid && post?.likes?.includes(user?.uid) ? (
+            <AiFillHeart />
+          ) : (
+            <AiOutlineHeart />
+          )}
           {post?.likeCount || 0}
         </button>
         <button type="button" className="post__comments">
